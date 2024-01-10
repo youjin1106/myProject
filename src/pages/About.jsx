6edbda23053ddee05e23467219df4f4e4c7dc3f5
Jang YouJin <jjnote22@gmail.com>
2024-01-10 18:06:00 +0900

    about 페이지 작성

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
 const About = () => {
   return (
-    <div>
-      <h1>Starbucks</h1>
-      {/* <img src={import.meta.env.VITE_PUBLIC_URL + "/starbucks_about.png"} /> */}
-      <span>스타벅스 바리스타를 위한 일정 관리</span>
+    <div className="about-page">
+      <img className="about-page__logo" src="../../public/starbucks_logo.png" />
+      <h1 className="about-page__intro-text">
+        스타벅스 바리스타를 위한 일정 관리
+      </h1>
+      {/* <img
+        src={import.meta.env.VITE_PUBLIC_URL + "/public/starbucks_about.png"}
+      /> */}
+      <img
+        className="about-page__intro-image"
+        src="../../public/starbucks_about.png"
+      />
     </div>
   );
 };
