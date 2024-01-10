8d0526f8d2416062309283fc2cc6c26eff768a97
Jang YouJin <jjnote22@gmail.com>
2024-01-10 17:43:56 +0900

    이미지 추가

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div>
       <h1>Starbucks</h1>
-      <>이미지</>
+      {/* <img src={import.meta.env.VITE_PUBLIC_URL + "/starbucks_about.png"} /> */}
       <span>스타벅스 바리스타를 위한 일정 관리</span>
     </div>
   );
